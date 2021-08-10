2cdd0eb556a460a19c61f9c724d72ec3ae5982f1
Dipesh Kumar <79369868+Uchiha-cpu@users.noreply.github.com>
2021-08-10 12:30:42 +0530

    Add files via upload

---
+++ aopconcepts/src/main/java/com/aop/services/PaymentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.aop.services;
+
+public class PaymentServiceImpl implements PaymentService {
+	
+	public void makePayment(int amount) {
+		System.out.println(amount+" Amount Debited..");
+		
+		
+		
+		
+		System.out.println(amount+" Amount Credited..");
+	}
+
+}
